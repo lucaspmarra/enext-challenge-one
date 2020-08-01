ee587ab105c34abf43dd68388675933ac4dc3627
Lucas Marra <marralucas1@gmail.com>
2020-08-01 01:40:03 -0300

    styling navbar

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ new Vue({
     data() {
         return {
             potions_list: [],
-            loading: true,
             errored: false
         }
     },
@@ -30,16 +29,16 @@ new Vue({
         axios
             .get('https://cdn.rawgit.com/LucasRuy/1d4a5d45e2ea204d712d0b324af28bab/raw/342e0e9277be486102543c7f50ef5fcf193234b6/potions.json')
             .then((response) => {
-                const {potions} = response.data;
+                const { potions } = response.data;
                 this.potions_list = potions;
                 console.log(potions);
-                
+
             })
             .catch(error => {
                 console.log(error)
                 this.errored = true
             })
-            .finally(() => this.loading = false)
+
     },
 
 })
