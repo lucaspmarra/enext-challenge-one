d1553b2f32fe0dd366f90c37f3e4414cfeca35f4
Lucas Marra <marralucas1@gmail.com>
2020-08-02 00:03:36 -0300

    commit simples

--- js/app.js
+++ js/app.js
@@ -9,22 +9,6 @@ new Vue({
     mounted: function () {
         this.getAllPotions();
     },
-    // methods: {
-    //     getAllPotions() {
-    //         axios
-    //             .get('https://cdn.rawgit.com/LucasRuy/1d4a5d45e2ea204d712d0b324af28bab/raw/342e0e9277be486102543c7f50ef5fcf193234b6/potions.json')
-    //             .then(function (response) {
-    //                 // console.log(response);
-    //                 if (response.data.error) {
-    //                     this.errored = true
-    //                 } else {
-    //                     console.log(this.potions_list = response.data.potions);
-    //                     this.potions_list = response.data.potions;
-    //                 }
-    //             })
-    //             .finally(() => this.loading = false)
-    //     }
-    // }
     mounted() {
         axios
             .get('https://cdn.rawgit.com/LucasRuy/1d4a5d45e2ea204d712d0b324af28bab/raw/342e0e9277be486102543c7f50ef5fcf193234b6/potions.json')
@@ -32,13 +16,10 @@ new Vue({
                 const { potions } = response.data;
                 this.potions_list = potions;
                 console.log(potions);
-
             })
             .catch(error => {
                 console.log(error)
                 this.errored = true
             })
-
     },
-
 })
